1601b1a77882a1b045b16a57bb4c8a8762d7f285
Anton Laurell <antonlaurell@hotmail.com>
2026-05-12 09:35:46 +0200

    compile shaders here

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-# naga --shader-stage compute --input-kind glsl shaders/compute.glsl shaders/compute.wgsl
-# naga --shader-stage vertex --input-kind glsl shaders/vertex.glsl shaders/vertex.wgsl
-# naga --shader-stage fragment --input-kind glsl shaders/fragment.glsl shaders/fragment.wgsl
+naga --shader-stage compute --input-kind glsl shaders/compute.glsl shaders/compute.wgsl
+naga --shader-stage vertex --input-kind glsl shaders/vertex.glsl shaders/vertex.wgsl
+naga --shader-stage fragment --input-kind glsl shaders/fragment.glsl shaders/fragment.wgsl
 emcc -s USE_WEBGPU=1 -s WASM_BIGINT=1 -s ASYNCIFY -s EXPORTED_RUNTIME_METHODS="['ccall']" --embed-file shaders/ main.c -o main.js
